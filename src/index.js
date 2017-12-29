d2b89e3468d957e03c25a984ca4d075eefea456e
Felipe Vargas <git@fvgs.ai>
2017-12-28 23:20:51 -0500

    Init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+	zero: require('./zero'),
+	one: require('./one'),
+	two: require('./two'),
+	three: require('./three'),
+	four: require('./four'),
+	five: require('./five'),
+	six: require('./six'),
+	seven: require('./seven'),
+	eight: require('./eight'),
+	nine: require('./nine'),
+	s: require('./s'),
+	space: require('./space'),
+	colon: require('./colon'),
+}
